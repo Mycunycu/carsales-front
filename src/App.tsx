70b6c6aa2a57cb5f6071f7c6d0cfb70eed2ac96f
mycunycu <mycunycu2607@gmail.com>
2021-02-03 00:20:18 +0300

    started registration page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { HomePage } from './views/pages/home';
+import { RegisterPage } from './views/pages/register';
 
 import './App.sass';
 
-function App() {
+export const App: FC = () => {
   return (
     <Router>
-      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/register' component={RegisterPage} />
     </Router>
   )
 }
-
-export default App;
---
+++ src/views/pages/register/index.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+
+import './index.sass';
+
+export const RegisterPage: FC = () => {
+  return (
+    <div className='registration'>
+      <div className='registration-container'>
+        <b>To Do Logo</b>
+        <p>Create Account</p>
+        <form>
+
+        </form>
+      </div>
+    </div>
+  )
+}
